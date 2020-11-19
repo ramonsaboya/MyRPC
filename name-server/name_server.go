0594277e6cf1893f2f82fa80761ac8da6282ef89
Rodrigo Cunha <rodrigo.cunha@manifestohut.com.br>
2020-11-18 21:16:19 -0300

    implement names service

--- name-server/name_server.go
+++ name-server/name_server.go
@@ -1,19 +1,41 @@
 package nameserver
 
-import "github.com/ramonsaboya/myrpc/commons"
+import (
+	"reflect"
+
+	"github.com/ramonsaboya/myrpc/commons"
+)
 
 type NameServer struct {
-	services map[string]commons.ClientProxy
+	services map[string]map[string]commons.ClientProxy
 }
 
-func (ns *NameServer) Register(name string, proxy commons.ClientProxy) (bool, error) {
+func (ns *NameServer) Register(name string, protocol string, proxy commons.ClientProxy) (bool, error) {
+	if ns.services[protocol] == nil {
+		ns.services[protocol] = map[string]commons.ClientProxy{}
+	}
 
+	ns.services[protocol][name] = proxy
+
+	return true, nil
 }
 
-func (ns *NameServer) Lookup(name string) (commons.ClientProxy, error) {
+func (ns *NameServer) Lookup(name string, protocol string) (commons.ClientProxy, error) {
+	if ns.services[protocol] == nil {
+		return commons.ClientProxy{}, nil
+	}
 
+	if reflect.ValueOf(ns.services[protocol][name]).IsNil() {
+		return commons.ClientProxy{}, nil
+	}
+
+	return ns.services[protocol][name], nil
 }
 
-func (ns *NameServer) List(name string) (map[string]commons.ClientProxy, error) {
+func (ns *NameServer) List(name string, protocol string) (map[string]commons.ClientProxy, error) {
+	if ns.services[protocol] == nil {
+		return map[string]commons.ClientProxy{}, nil
+	}
 
+	return ns.services[protocol], nil
 }
