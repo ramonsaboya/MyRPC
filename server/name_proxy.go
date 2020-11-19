4251b7cbdfef234aadf089e6bd2cc0f3b8d76785
Rodrigo Cunha <rodrigo.cunha@manifestohut.com.br>
2020-11-19 17:19:52 -0300

    add name proxy

---
+++ server/name_proxy.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"github.com/ramonsaboya/myrpc/client"
+	"github.com/ramonsaboya/myrpc/commons"
+)
+
+type NameProxy struct {
+	proxy commons.ClientProxy
+}
+
+func NewNamingProxy(proxyRef *commons.ClientProxy) (*NameProxy, error) {
+	return &NameProxy{
+		proxy: *proxyRef,
+	}, nil
+}
+
+func (n *NameProxy) Register(proxy commons.ClientProxy) (*bool, error) {
+	req := client.Request{
+		Operation:   "Register",
+		Proxy:       proxy,
+		ServiceName: "Calculator",
+	}
+
+	requestor, err := client.NewRequestor(&n.proxy)
+	if err != nil {
+		return nil, err
+	}
+	res, err := requestor.Invoke(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	reply := res.(bool)
+
+	return &reply, nil
+}
+
+func (n *NameProxy) Lookup(name string) (*commons.ClientProxy, error) {
+	req := client.Request{
+		Operation:   "Lookup",
+		ServiceName: "Calculator",
+	}
+
+	requestor, err := client.NewRequestor(&n.proxy)
+	if err != nil {
+		return nil, err
+	}
+	res, err := requestor.Invoke(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	reply := res.(commons.ClientProxy)
+
+	return &reply, nil
+}
